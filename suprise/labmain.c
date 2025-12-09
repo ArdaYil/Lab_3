4ce44b58d1d26117e0d31ae9e1ba9287852374fa
is1500 <is1500@IS1500-Debian-12.myguest.virtualbox.org>
2025-12-09 22:39:21 +0100

    a

--- suprise/labmain.c
+++ suprise/labmain.c
@@ -62,7 +62,7 @@ void set_displays(int display_number, int value) {
 void handle_interrupt(unsigned cause) {
     print_dec(cause);
     volatile int *timer_status = (volatile int *)(0x04000020);
-    volatile int *btn_edge     = (volatile int *)(0x040000dc);
+    volatile int *btn_edge = (volatile int *)(0x040000d0 + 0xC);
     
     // ==========================================
     // 1. CHECK TIMER (Address 0x04000020)
