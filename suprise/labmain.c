b198496e7899347f60b5a71d4df17f51d127a18e
is1500 <is1500@IS1500-Debian-12.myguest.virtualbox.org>
2025-12-09 22:35:14 +0100

    a

--- suprise/labmain.c
+++ suprise/labmain.c
@@ -96,7 +96,6 @@ void handle_interrupt(unsigned cause) {
     if (cause == 18) {
         // 1. Read the state to decide what logic to run
         int edge_val = *btn_edge;
-        *btn_edge = 0;
         
         // 2. ACKNOWLEDGE IMMEDIATELY with a heavy hammer.
         // Writing 0xFF clears ALL pending edge bits (Bit 0, 1, 2...).
@@ -104,6 +103,7 @@ void handle_interrupt(unsigned cause) {
 
         // 3. Logic: Check if Button 2 (Bit 1) was the one pressed
         if (get_btn()) {
+            *btn_edge = 0;
             // A. Increment by 2 seconds
             seconds += 2;
 
